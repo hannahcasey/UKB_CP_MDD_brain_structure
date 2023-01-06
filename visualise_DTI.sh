b7d6f10291eb503778aee2f9a440e176ccccbb60
Hannah Casey <53822789+hannahcasey@users.noreply.github.com>
2023-01-06 10:17:33 +0000

    update

--- visualise_DTI.sh
+++
@@ -1,7 +0,0 @@
-## Dowload example DTI data: https://www.fmrib.ox.ac.uk/primers/intro_primer/ExBox6/IntroBox6.html
-
-## Carry out preprocessing, tractography and visualisation using autoptx:
-
-## Use nodif_brain_mask.nii.gz to create surface in MANGO
-
-## Get hex number related to beta value of DTI tracts and input to MANGO
