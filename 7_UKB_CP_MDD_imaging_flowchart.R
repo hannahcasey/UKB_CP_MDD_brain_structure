500169b2f2b30b84d44a5ab8e7811be23283e1e0
Hannah Casey <53822789+hannahcasey@users.noreply.github.com>
2024-10-23 22:25:00 +0100

    As neurological to flowchart

--- 7_UKB_CP_MDD_imaging_flowchart.R
+++ 7_UKB_CP_MDD_imaging_flowchart.R
@@ -7,30 +7,36 @@ library(dplyr)
 library(ggh4x)
 
 ## Load in data ----
-full_data <- read.csv("/Volumes/GenScotDepression/users/hcasey/UKB_brain_structure_CP_MDD/UKB_imaging_covariates_qc.csv")
+full_data <- read.csv("/Volumes/GenScotDepression/users/hcasey/UKB_brain_structure_CP_MDD/UKB_imaging_covariates.csv")
 non_british_irish <- read.csv("/Volumes/GenScotDepression/users/hcasey/UKB_brain_structure_CP_MDD/non_british_irish.csv")
+UKB_neurological <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_CP_MDD_brain_structure/resources/UKB_neurological.csv")
+UKB_neurological <- subset(UKB_neurological, neurological == 1)
 UKB_CP <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_chronic_pain_phenotype/output/UKB_chronic_pain_phenotype_imaging.csv")
 UKB_MDD <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_depression_phenotype/output/UKB_depression_phenotype.csv")
 
 ## Sample flow chart ----
 UKB_with_imaging <- full_data$f.eid[!is.na(full_data$ICV)] ## IDs with imaging - outliers removes on a case by case basis
 UKB_british_irish_with_imaging <- UKB_with_imaging[!UKB_with_imaging %in% non_british_irish$x] ## IDs british/irish with imaging
+UKB_british_irish_with_imaging_non_neurological <- UKB_british_irish_with_imaging[!UKB_british_irish_with_imaging %in% UKB_neurological$f_eid] ## IDs non neurological british/irish with imaging
+
 UKB_non_british_irish_with_imaging <- UKB_with_imaging[UKB_with_imaging %in% non_british_irish$x] ## IDs non british/irish in imaging sample
-UKB_non_british_irish_with_imaging_CP <- UKB_CP$n_eid[(UKB_CP$n_eid %in% UKB_british_irish_with_imaging) & !is.na(UKB_CP$chronic_pain_status)] ## IDs british/irish in imaging sample with CP phenotyping
-UKB_non_british_irish_with_imaging_MDD <- UKB_MDD$f_eid[(UKB_MDD$f_eid %in% UKB_british_irish_with_imaging) & !is.na(UKB_MDD$recurrent_depression)] ## IDs british/irish in imaging sample with MDD phenotyping
-UKB_non_british_irish_with_imaging_CPMDD <- UKB_non_british_irish_with_imaging_MDD[UKB_non_british_irish_with_imaging_MDD %in% UKB_non_british_irish_with_imaging_CP]
+UKB_neurological_british_irish_with_imaging <- UKB_non_british_irish_with_imaging[UKB_non_british_irish_with_imaging %in% UKB_neurological$f_eid] ## IDs neurological British/Irish in imaging sample
+
+UKB_non_british_irish_with_imaging_non_neurological_CP <- UKB_CP$n_eid[(UKB_CP$n_eid %in% UKB_british_irish_with_imaging_non_neurological) & !is.na(UKB_CP$chronic_pain_status)] ## IDs british/irish in imaging sample with CP phenotyping
+UKB_non_british_irish_with_imaging_non_neurological_MDD <- UKB_MDD$f_eid[(UKB_MDD$f_eid %in% UKB_british_irish_with_imaging_non_neurological) & !is.na(UKB_MDD$recurrent_depression)] ## IDs british/irish in imaging sample with MDD phenotyping
+UKB_non_british_irish_with_imaging_non_neurological_CPMDD <- UKB_non_british_irish_with_imaging_non_neurological_MDD[UKB_british_irish_with_imaging_non_neurological %in% UKB_non_british_irish_with_imaging_non_neurological_CP]
 
 
 UKB_pop <- boxGrob(glue("UK Biobank Sample",
                         "n = {pop}",
-                        pop = txtInt(502357),
+                        pop = txtInt(nrow(full_data)),
                         .sep = "\n"), 
                    y = 0.9, x = 0.5, bjust = c(0.5, 0.5),
                    just = "centre")
 
-eligible_pop <- boxGrob(glue("British/Irish with Imaging",
+eligible_pop <- boxGrob(glue("British/Irish with Imaging and no Neurological Conditions",
                              "n = {pop}",
-                             pop = txtInt(length(UKB_british_irish_with_imaging)),
+                             pop = txtInt(length(UKB_british_irish_with_imaging_non_neurological)),
                              .sep = "\n"), 
                         y = 0.6, x = 0.5, bjust = c(0.5, 0.5),
                         just = "centre")
@@ -38,30 +44,32 @@ eligible_pop <- boxGrob(glue("British/Irish with Imaging",
 exclude_pop <- boxGrob(glue("Excluded (n = {tot}):",
                             " - No Imaging: {no_imaging}",
                             " - Non-British/Irish (in no imaging sample): {non_british_irish}",
-                            tot = txtInt(502357 - length(UKB_british_irish_with_imaging)),
-                            no_imaging = txtInt(502357 - length(UKB_with_imaging)),
+                            " - Neurological condition (in no imaging non British/Irish sample): {neurolocial}",
+                            tot = txtInt(nrow(full_data) - length(UKB_british_irish_with_imaging_non_neurological)),
+                            no_imaging = txtInt(nrow(full_data) - length(UKB_with_imaging)),
                             non_british_irish = txtInt(length(UKB_non_british_irish_with_imaging)),
+                            neurolocial = txtInt(length(UKB_neurological_british_irish_with_imaging)),
                             .sep = "\n"), 
                        y = 0.75, x = 0.75, bjust = c(0.5, 0.5),
                        just = "left")
 
 CP_pop <- boxGrob(glue("Chronic Pain Phenotyping",
                        "n = {pop}",
-                       pop = txtInt(length(UKB_non_british_irish_with_imaging_CP)),
+                       pop = txtInt(length(UKB_non_british_irish_with_imaging_non_neurological_CP)),
                        .sep = "\n"), 
                   y = 0.3, x = 0.3, bjust = c(0.5, 0.5),
                   just = "centre")
 
 MDD_pop <- boxGrob(glue("Depression Phenotyping",
                        "n = {pop}",
-                       pop = txtInt(length(UKB_non_british_irish_with_imaging_MDD)),
+                       pop = txtInt(length(UKB_non_british_irish_with_imaging_non_neurological_MDD)),
                        .sep = "\n"), 
                   y = 0.3, x = 0.7, bjust = c(0.5, 0.5),
                   just = "centre")
 
 CPMDD_pop <- boxGrob(glue("Both Chronic Pain and Depression Phenotyping",
                         "n = {pop}",
-                        pop = txtInt(length(UKB_non_british_irish_with_imaging_CPMDD)),
+                        pop = txtInt(length(UKB_non_british_irish_with_imaging_non_neurological_CPMDD)),
                         .sep = "\n"), 
                    y = 0.2, x = 0.5, bjust = c(0.5, 0.5),
                    just = "centre")
