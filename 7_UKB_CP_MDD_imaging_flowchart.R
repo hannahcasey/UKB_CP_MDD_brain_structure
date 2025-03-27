82bfc758afe2c9dd0b2ece626bab025656467fcb
Hannah Casey <53822789+hannahcasey@users.noreply.github.com>
2025-03-27 15:35:00 +0000

    Update depression phenotype

--- 7_UKB_CP_MDD_imaging_flowchart.R
+++ 7_UKB_CP_MDD_imaging_flowchart.R
@@ -7,12 +7,13 @@ library(dplyr)
 library(ggh4x)
 
 ## Load in data ----
-full_data <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_CP_MDD_brain_structure/resources/UKB_imaging_covariates_qc.csv")
-UKB_exclude <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_CP_MDD_brain_structure/resources/w4844_20241216.csv", header = F)
-UKB_imaging_outliers <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_CP_MDD_brain_structure/resources/imaging_outliers.csv")
-UKB_neurological <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_CP_MDD_brain_structure/resources/UKB_neurological.csv")
-UKB_CP <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_chronic_pain_phenotype/output/UKB_chronic_pain_phenotype_imaging.csv")
-UKB_MDD <- read.csv("/Users/hannahcasey/Desktop/PhD/projects/UKB_CP_MDD_brain_structure/resources/UKB_current_depression_imaging.csv")
+#work_dir <- ""
+full_data <- read.csv(paste0(work_dir,"/resources/UKB_imaging_covariates_qc.csv"))
+UKB_exclude <- read.csv(paste0(work_dir,"/resources/w4844_20241216.csv"), header = F)
+UKB_imaging_outliers <- read.csv(paste0(work_dir,"/resources/imaging_outliers.csv"))
+UKB_neurological <- read.csv(paste0(work_dir,"/resources/UKB_neurological.csv"))
+UKB_CP <- read.csv(paste0(work_dir,"/../UKB_chronic_pain_phenotype/output/UKB_chronic_pain_phenotype_imaging.csv"))
+UKB_MDD <- read.csv(paste0(work_dir,"/resources/UKB_current_depression_imaging.csv"))
 
 ## Remove excluded
 full_data <- full_data[!full_data$f.eid %in% UKB_exclude$V1,]
@@ -47,12 +48,17 @@ n_outliers_non_neurological_in_imaging <- n_non_neurological_in_imaging - nrow(e
 ## With chronic pain phenotyping
 UKB_CP <- UKB_CP[UKB_CP$n_eid %in% eligible_data$f.eid,]
 n_CP <- length(na.omit(UKB_CP$chronic_pain_sites))
-CP_IDs <- UKB_CP$n_eid[!is.na(UKB_CP$chronic_pain_sites)]
+CP_IDs <- UKB_CP$n_eid[!is.na(UKB_CP$chronic_pain_status)]
+n_CP_missing_phenotype <- sum(!eligible_data$f.eid %in% CP_IDs) ## N without chronic pain phenotyping
 
 ## With depression phenotyping
+n_MDD_missing_phenotype <- sum(eligible_data$f.eid %in% UKB_MDD$f.eid[(is.na(UKB_MDD$smith_depression) | is.na(UKB_MDD$current_depression))]) ## N without depression phenotyping
+n_MDD_current_no_liftime <- sum(eligible_data$f.eid %in% UKB_MDD$f.eid[UKB_MDD$smith_depression == 0 & UKB_MDD$current_depression == 1])
+n_MDD_excluded <- n_MDD_missing_phenotype + n_MDD_current_no_liftime
 UKB_MDD <- UKB_MDD[UKB_MDD$f.eid %in% eligible_data$f.eid,]
-n_MDD <- length(na.omit(UKB_MDD$current_depression))
-MDD_IDs <- UKB_MDD$f.eid[!is.na(UKB_MDD$current_depression)]
+n_MDD <- length(na.omit(UKB_MDD$depression))
+MDD_IDs <- UKB_MDD$f.eid[!is.na(UKB_MDD$depression)]
+
 
 ## With chronic pain and depression phenotyping
 n_MDD_CP <- length(intersect(MDD_IDs, CP_IDs))
@@ -84,18 +90,38 @@ exclude_pop <- boxGrob(glue("Excluded (n = {tot}):",
                        y = 0.75, x = 0.75, bjust = c(0.5, 0.5),
                        just = "left")
 
+
+CP_excluded_pop <- boxGrob(glue("Excluded (n = {tot})",
+                                 "- Missing chronic pain phenotyping",
+                                 tot = txtInt(n_CP_missing_phenotype),
+                                 .sep = "\n"), 
+                            y = 0.4, x = 0.15, bjust = c(0.5, 0.5),
+                            just = "left")
+
 CP_pop <- boxGrob(glue("Chronic Pain Phenotyping:",
                        "n = {pop}",
                        pop = txtInt(n_CP),
                        .sep = "\n"), 
-                  y = 0.4, x = 0.3, bjust = c(0.5, 0.5),
+                  y = 0.4, x = 0.4, bjust = c(0.5, 0.5),
                   just = "centre")
+
+
+MDD_excluded_pop <- boxGrob(glue("Excluded (n = {tot})",
+                                 "- Missing depression phenotyping: {no_pheno}",
+                                 "- Current depression symptoms, \nno lifetime depression: {current_no_liftime}",
+                        tot = txtInt(n_MDD_excluded),
+                        no_pheno = txtInt(n_MDD_missing_phenotype),
+                        current_no_liftime = txtInt(n_MDD_current_no_liftime),
+                        .sep = "\n"), 
+                   y = 0.4, x = 0.87, bjust = c(0.5, 0.5),
+                   just = "left")
+
 
 MDD_pop <- boxGrob(glue("Depression Phenotyping:",
                        "n = {pop}",
                        pop = txtInt(n_MDD),
                        .sep = "\n"), 
-                  y = 0.4, x = 0.7, bjust = c(0.5, 0.5),
+                  y = 0.4, x = 0.6, bjust = c(0.5, 0.5),
                   just = "centre")
 
 CPMDD_pop <- boxGrob(glue("Both Chronic Pain and Depression Phenotyping:",
@@ -106,22 +132,27 @@ CPMDD_pop <- boxGrob(glue("Both Chronic Pain and Depression Phenotyping:",
                    just = "centre")
 
 grid.newpage()
-jpeg("~/Desktop/PhD/projects/UKB_CP_MDD_brain_structure/output/flowchart.jpg", width = 10000, height = 8000, res = 1000)
+jpeg("~/Desktop/PhD/projects/UKB_CP_MDD_brain_structure/output/flowchart.jpg", width = 13000, height = 8000, res = 1000)
 
-
-UKB_pop
-eligible_pop
-exclude_pop
-CP_pop
-MDD_pop
-CPMDD_pop
 
 connectGrob(UKB_pop, eligible_pop, "N")
 connectGrob(UKB_pop, exclude_pop, "L")
 connectGrob(eligible_pop, MDD_pop, "N")
 connectGrob(eligible_pop, CP_pop, "N")
 connectGrob(MDD_pop, CPMDD_pop, "N")
+connectGrob(MDD_pop, MDD_excluded_pop, "L")
 connectGrob(CP_pop, CPMDD_pop, "N")
+connectGrob(CP_pop, CP_excluded_pop, "L")
+
+UKB_pop
+eligible_pop
+exclude_pop
+CP_pop
+MDD_excluded_pop
+MDD_pop
+CP_excluded_pop
+CPMDD_pop
+
 
 dev.off()
 
